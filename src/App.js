a4de10aa1057339499198005997c2a0a10896dd4
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2024-03-01 02:29:31 +0600

    Showed data using conditional rendering with ternary operator

--- src/App.js
+++ src/App.js
@@ -65,28 +65,53 @@ function Header() {
 }
 
 function Menu() {
+  // const pizzas = [];
+  const pizzas = pizzaData;
+  const numPizza = pizzas.length;
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza></Pizza>
+      {numPizza > 0 ? (
+        <ul className='pizzas'>
+          {pizzas.map((pizza) => (
+            <Pizza key={pizza.name} pizzaObj={pizza} />
+          ))}
+        </ul>
+      ) : <p>We're still working on our menu. Please come back later :)</p>}
     </main>
   )
 }
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
+  const openHour = 10;
+  const closeHour = 20;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-
-  // if(hour >= openHour && hour <= closeHour) alert("We're currently open!")
-  // else alert("Sorry we are closed.")
-  // console.log(hour)
-  return <footer className='footer'>{new Date().toLocaleTimeString()} We're currently open</footer>
+  return (
+    <footer className='footer'>
+      {isOpen ? (
+        <div className='order'>
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.
+          </p>
+          <button className='btn'>Order</button>
+        </div>
+      ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>}
+    </footer>
+  )
 }
 
-function Pizza() {
-  return <h3>Pizza</h3>
+function Pizza({ pizzaObj }) {
+  const { photoName, name, ingredients, price } = pizzaObj;
+  return <>
+    <li className='pizza'>
+      <img src={photoName} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{price}</span>
+      </div>
+    </li>
+  </>
 }
 
 export default App;
